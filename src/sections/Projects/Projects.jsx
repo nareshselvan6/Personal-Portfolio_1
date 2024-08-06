d4aa4ecbbab5b175d33e1ed3cecfc7f8ffa80f53
nareshselvan6 <nareshselvan6@gmail.com>
2024-08-06 23:50:12 +0530

    FSD projects added

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,9 @@
 import styles from './ProjectsStyles.module.css';
 import weather from "../../assets/weather.jpg";
 import carcare from"../../assets/carcare.png"
+import coffeeshop from "../../assets/coffeeshopimg.jpg"
+import library from "../../assets/library2.jpg"
+import todolist from "../../assets/todolist.png"
 import url from '../../assets/url.jpg';
 import ProjectCard from '../../common/ProjectCard';
 
@@ -9,28 +12,35 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
+
+      <ProjectCard
+          src={todolist}
+          link="https://todolist-five-lyart.vercel.app/"
+          h3="TODO LIST"
+          p="Fullstack Entry Level project"
+        />
+<ProjectCard
+  src={library}
+  link="https://librarymanagement-rct.netlify.app/"
+  h3="Library Management"
+  p="Fullstack Intermediate Level project"
+/>
+
         
+<ProjectCard
+  src={coffeeshop}
+  link="https://coffee-shop-frontend-vert.vercel.app/"
+  h3="Coffee Shop"
+  p="Fullstack Professional Level project"
+/>
       <ProjectCard
           src={carcare}
           link="https://frontend-capstone-vehiclecare.netlify.app/"
           h3="TS CAR_CARE"
-          p="FULLSTACK PROJECT"
+          p="FULLSTACK Company Level PROJECT"
         />
 
-        <ProjectCard
-          src={weather}
-          link="https://weatherapi-kappa.vercel.app/"
-          h3="Weather-API"
-          p="FRONTEND PROJECT_WITH_API"
-        />
-      
-        <ProjectCard
-          src={url}
-          link="https://url-shortner-frontend-three-eta.vercel.app/"
-          h3="URL_SHORTNER"
-          p="FRONTEND_WITH_BACKEND PROJECT"
-        />
-
+    
       </div>
     </section>
   );
